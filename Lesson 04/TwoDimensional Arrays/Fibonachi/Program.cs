a95587e2cbc05d79d808d8bd1959376c2f4940d0
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-28 09:34:30 +0300

    Рекурсия фибоначи

---
+++ Lesson 04/TwoDimensional Arrays/Fibonachi/Program.cs
@@ -0,0 +1,15 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    //   || это ИЛИ
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 50; i++)
+    // обязательно i = 1 , так как это не массив 
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
