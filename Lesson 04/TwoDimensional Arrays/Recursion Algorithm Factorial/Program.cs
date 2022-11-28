52cd55c9ffc55901581863d2c2a28a7fc9cfdbc5
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-28 09:36:09 +0300

    Рекурсия Факториал

---
+++ Lesson 04/TwoDimensional Arrays/Recursion Algorithm Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factorial(int n)
+{
+    // 1!(1 факториал)= 1
+    // 0! (0 факториал) = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+// Console.WriteLine(Factorial(5)); // Факториал 3х = 1*2*3 = 6
